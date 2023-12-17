e8667b302dada93bb6151763829c63b0b73c50e5
ahmed ayman <ahmed3ayman3@gmail.com>
2023-12-17 20:12:46 +0200

    init

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+#define ll long long
+#define endl "\n"
+#define all(v) v.begin(),v.end()
+using namespace std;
+
+int main() {
+    fast
+    int t = 1;
+    //cin>>t;
+    while (t--)
+    {
+        
+    }
+    return 0;
+}
